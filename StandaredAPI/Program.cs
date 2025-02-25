44c802f2c26db9c27b96842ab94be7bf82e086d7
Tayyab94 <tayyab.bhatti30@gmail.com>
2025-02-25 18:00:51 +0500

    first commit

---
+++ StandaredAPI/Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+List<Person> persons = [
+    new Person("John", "Doe", 25),
+     new Person("Jane", "Dam", 20),
+     new Person("John", "Smith", 25),
+     new Person("Ali","Ahmad",14),
+       new Person("Jane", "Smith", 25),
+    ];
+
+app.MapGet("/persons", () => persons);
+app.MapGet("/persons/{id}", (int id) => persons[id]);
+app.MapPost("/persons",(Person model) =>
+{
+    persons.Add(model);
+    return persons;
+});
+
+
+app.MapPut("/persons/{id}", (int id, Person model) =>
+{
+    persons[id] = model;
+    return persons;
+});
+
+
+app.MapDelete("/persons/{id}", (int id) =>
+{
+    persons.RemoveAt(id);
+});
+
+
+app.Run();
+
+
+public record Person(string FirstName, string LastName, int age);
